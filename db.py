4e64510a7aa8a63dd60f6abced4a16daa8a818c7
Douglas R. <114185919+doug-folk@users.noreply.github.com>
2025-06-01 13:56:55 -0300

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 Base = declarative_base()
 
 class Database:
-    def __init__(self, url="postgresql+psycopg2://postgres:root@localhost:5432/escola"):
+    def __init__(self.engine = create_engine("postgresql://usuario:senha@localhost/escola"):
         self.engine = create_engine(url, echo=False, future=True)
         self.Session = sessionmaker(bind=self.engine, future=True)
 
